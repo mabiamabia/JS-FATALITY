e11504b38e8f76eb720cd2863bea3336d8bfc1f1
mabiamabia <duartecostap@gmail.com>
2023-01-21 20:03:09 -0300

    feat: reverse types

---
+++ CursoJavascriptV2/Revisao-logica/conversao-tipos.js
@@ -0,0 +1,12 @@
+// parseFloat, parseInt, Number()
+// decimais 0 - 9
+//hexadecimais 0 - f (15)
+
+let n1 = "12";
+n1 = Number(n1);
+
+let n2 = 1234;
+n2 = n2.toString(2);
+
+console.log(n2, typeof n2);
+console.log(n1 * n2, typeof n1, typeof n2, n1);
