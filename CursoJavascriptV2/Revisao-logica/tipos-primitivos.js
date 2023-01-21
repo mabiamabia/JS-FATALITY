9adf78a8e8cc4e10342b4af2c17115caa0fe4eb9
mabiamabia <duartecostap@gmail.com>
2023-01-21 19:28:06 -0300

    feat: add primitive types

---
+++ CursoJavascriptV2/Revisao-logica/tipos-primitivos.js
@@ -0,0 +1,23 @@
+// strings, number(int, floats), boolean
+// undefined, null, symbol (ES2015)
+
+let minha_var = "oloco meu 'com aspas'";
+let minhaVar2 = 'minha "string" com aspas duplas';
+let minhaVar3 = 'nova "string" com aspas duplas';
+var minhavar3 = `minha template literal`;
+let idade = 40;
+let msg = `eu possuo ${idade} anos`;
+let msg2 = "eu possuo " + idade + " anosss";
+let varTeste;
+varTeste = 10;
+let varteste2 = null;
+
+console.log(minha_var);
+console.log(minhaVar2);
+console.log(minhaVar3);
+console.log(minhavar3);
+console.log(msg);
+console.log(msg2);
+console.log(varTeste);
+console.log(typeof varTeste);
+console.log(typeof varteste2);
---
+++ CursoJavascriptV2/Revisao-logica/variaveis.js
@@ -0,0 +1,22 @@
+// var - let - const
+// var -> ES5 - cross-browser
+// let e const -> ES2015
+
+let teste = "minha string";
+const teste2 = 30;
+let teste3;
+teste3 = 20;
+const teste4 = 40;
+const n1 = 13;
+const n2 = 1.1;
+const isValid = true;
+const isFalse = false;
+
+console.log(teste2);
+console.log(teste3);
+console.log(teste4);
+console.log(typeof teste, typeof teste3, typeof teste2, "--", 20);
+console.log(`o tipo de n1 é ${typeof n1} e seu valor é ${n1}`);
+console.log(`o tipo de n2 é ${typeof n2} e seu valor é ${n2}`);
+console.log(`isValid: ${isValid}`);
+console.log(`isFalse: ${isFalse}`);
